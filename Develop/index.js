17022e32ac2f947ccc547c9b3b8785e37d8f14db
jester <jaimebuddy98@gmail.com>
2022-01-18 13:25:24 -0600

    added packages at the top of the page

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,19 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application ------------------------------------------------
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
+// TODO: Create an array of questions for user input -------------------------------------------------
 const questions = [];
 
-// TODO: Create a function to write README file
+
+// TODO: Create a function to write README file ------------------------------------------------------
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+
+// TODO: Create a function to initialize app ---------------------------------------------------------
 function init() {}
 
-// Function call to initialize app
+
+// Function call to initialize app -------------------------------------------------------------------
 init();
