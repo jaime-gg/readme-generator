207d9b439ba25840d1950ad8abff1981877e64b6
jester <jaimebuddy98@gmail.com>
2022-01-18 13:59:30 -0600

    included installation question

--- Develop/index.js
+++ Develop/index.js
@@ -19,6 +19,13 @@ const questions = [
         name: 'description',
     },
 
+    // ##INSTALATION 
+    {
+        type: 'input',
+        message: 'What are the installation instructions? ',
+        name: 'installInstructions',
+    },
+
     // ##USAGE
     {
         type: 'input',
@@ -66,11 +73,15 @@ const questions = [
 
 
 // TODO: Create a function to write README file ------------------------------------------------------
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 
 // TODO: Create a function to initialize app ---------------------------------------------------------
-function init() {}
+function init() {
+
+}
 
 
 // Function call to initialize app -------------------------------------------------------------------
