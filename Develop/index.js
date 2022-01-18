b23c8e4727228a3274a88e9b662318120bbb950a
jester <jaimebuddy98@gmail.com>
2022-01-18 13:56:04 -0600

    created an array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,65 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input -------------------------------------------------
-const questions = [];
+const questions = [
+    // #TITLE
+    {
+        type: 'input',
+        message: 'What is the project title? ',
+        name: 'title',
+    },
+
+    // ##DESCRIPTION
+    {
+        type: 'input',
+        message: 'Please include a detailed description.',
+        name: 'description',
+    },
+
+    // ##USAGE
+    {
+        type: 'input',
+        message: 'What is the best way to use this project/application?',
+        name: 'usage',
+    },
+
+    // ##CONTRIBUTIONS
+    {
+        type: 'input',
+        message: 'Who/What contrubuted to the developement of this project? ',
+        name: 'contribution',
+    },
+
+    // ##TESTING
+    {
+        type: 'input',
+        message: 'How would one test the project application? ',
+        name: 'test',
+    },
+
+    // ##LICENSE 
+    // TODO: include list of licenses
+    {
+        type: 'list',
+        message: 'Under what license is this project? ',
+        name: 'license',
+        choices: [],
+    },
+
+    // ##QUESTIONS | GITHUB
+    {
+        type: 'input',
+        message: 'What is your GitHub username? ',
+        name: 'userName',
+    },
+
+    // ##QUESTIONS | EMAIL
+    {
+        type: 'input',
+        message: 'What is your email address? ',
+        name: 'email',
+    },
+];
 
 
 // TODO: Create a function to write README file ------------------------------------------------------
